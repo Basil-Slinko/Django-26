74290f70abd76b3500bbc6db077df38c6a89dc90
Basil-Slinko <basil.slinko@gmail.com>
2022-04-14 22:04:22 +0300

    HW
    migrate 2

--- Homework/2.2-databases-2/orm_migrations/school/views.py
+++ Homework/2.2-databases-2/orm_migrations/school/views.py
@@ -6,12 +6,13 @@ from .models import Student, Teacher
 
 def students_list(request):
     template = 'school/students_list.html'
+    # ordering = 'name', 'subject'
     context = {
-        Student.teachers: Teacher.objects.order_by('name')
+        Student.teachers: Teacher.objects.prefetch_related('teachers').order_by('name', 'subject').all()
     }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # ordering = 'group'
 
     return render(request, template, context)
