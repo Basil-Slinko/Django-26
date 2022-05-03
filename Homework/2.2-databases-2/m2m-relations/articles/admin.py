458f09d2b339abc98fbafd9ef39cafe643bf1e5b
Basil-Slinko <basil.slinko@gmail.com>
2022-05-03 18:43:34 +0300

    hw - m2m

--- Homework/2.2-databases-2/m2m-relations/articles/admin.py
+++ Homework/2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
-from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 
 from .models import Article, TagArticles
 
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                counter += 1
+        if counter == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif counter > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()
+
+
 class ArticleScopeInline(admin.TabularInline):
     model = TagArticles
-    extra = 3
+    formset = RelationshipInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
@@ -16,18 +29,3 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'published_at', 'image', ]
     search_fields = ['text']
     inlines = [ArticleScopeInline]
-
-    # def clean(self):
-    #     for form in self.forms:
-    #         '''В form.cleaned_data будет словарь с данными
-    #         каждой отдельной формы, которые вы можете проверить'''
-    #         print('-------')
-    #         print('-------')
-    #         form.cleaned_data
-    #         print('-------')
-    #         print('-------')
-    #         '''вызовом исключения ValidationError можно указать админке о наличие ошибки
-    #         таким образом объект не будет сохранен,
-    #         а пользователю выведется соответствующее сообщение об ошибке'''
-    #         raise ValidationError('Тут всегда ошибка')
-    #     return super().clean()  # вызываем базовый код переопределяемого метода
--- Homework/2.2-databases-2/m2m-relations/articles/views.py
+++ Homework/2.2-databases-2/m2m-relations/articles/views.py
@@ -1,25 +1,21 @@
+from pprint import pprint
+
 from django.shortcuts import render
 
 from articles.models import Article
 
 
-# def articles_list(request):
-#     context = {
-#         'object_list': Article.objects.order_by('-published_at').all()
-#     }
-#     return render(request, 'articles/news.html', context)
 def articles_list(request):
-    template = 'articles/news.html'
+    pprint(Article.objects.scopes.all())
     articles = Article.objects.all().prefetch_related('scopes').order_by('-published_at')
 
     for article in articles:
         for scope in article.scopes.all():
-            article_set = scope.tag.get(article=article)
-            scope.is_main = article_set.is_main
+            articleset = scope.get(article=article)
+            scope.is_main = articleset.is_main
 
-    context = {'object_list': articles}
 
-    ordering = '-published_at'
-
-    return render(request, template, context)
-
+    context = {
+        'object_list': articles
+    }
+    return render(request, 'articles/news.html', context)
